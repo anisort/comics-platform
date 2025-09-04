deda4fb99d9ff4a0a718662608fd48d4986593ba
anisort <jaki.anisort@gmail.com>
2025-09-04 15:37:17 +0300

    fix import and usage of schedule

--- comics-platform-backend/src/cleanup/cleanup.service.ts
+++ comics-platform-backend/src/cleanup/cleanup.service.ts
@@ -1,6 +1,6 @@
 import { Injectable, OnModuleInit } from '@nestjs/common';
 import { CleanupFactory } from './cleanup.factory';
-import cron from 'node-cron';
+import { schedule } from 'node-cron';
 
 @Injectable()
 export class CleanupService implements OnModuleInit {
@@ -9,9 +9,8 @@ export class CleanupService implements OnModuleInit {
   onModuleInit() {
     this.factories.forEach((factory) => {
       const task = factory.createCleanupTask();
-      // eslint-disable-next-line @typescript-eslint/no-misused-promises
-      cron.schedule('* * * * *', async () => {
-        await task.cleanup();
+      schedule('* * * * *', () => {
+        void task.cleanup();
       });
     });
   }
