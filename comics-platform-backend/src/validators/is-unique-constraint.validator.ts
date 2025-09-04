91b8c277eaa3cf75318fa1c9d5aad856c80d4e08
anisort <jaki.anisort@gmail.com>
2025-09-04 14:29:41 +0300

    fix all ESLint issues in validators folder

--- comics-platform-backend/src/validators/is-unique-constraint.validator.ts
+++ comics-platform-backend/src/validators/is-unique-constraint.validator.ts
@@ -1,25 +1,40 @@
-import { ValidatorConstraintInterface, ValidationArguments, ValidatorConstraint } from "class-validator";
-import { IsUniqueConstraintInput } from "./is-unique";
+import {
+  ValidatorConstraintInterface,
+  ValidationArguments,
+  ValidatorConstraint,
+} from 'class-validator';
+import { IsUniqueConstraintInput } from './is-unique';
 import { EntityManager } from 'typeorm';
-import { Injectable } from "@nestjs/common";
+import { Injectable } from '@nestjs/common';
 
-@ValidatorConstraint({name: 'IsUniqueConstraint', async: true})
+@ValidatorConstraint({ name: 'IsUniqueConstraint', async: true })
 @Injectable()
 export class IsUniqueConstraint implements ValidatorConstraintInterface {
+  constructor(private readonly entityManager: EntityManager) {}
 
-    constructor(private readonly entityManager: EntityManager){}
+  async validate(value: any, args?: ValidationArguments): Promise<boolean> {
+    const constraint = args?.constraints?.[0] as IsUniqueConstraintInput;
+    if (!constraint) return false;
 
-    async validate(
-        value: any,
-        args?: ValidationArguments,
-    ): Promise<boolean> {
-        const { tableName, column}: IsUniqueConstraintInput = args?.constraints[0];
-        const exists = await this.entityManager.getRepository(tableName).createQueryBuilder(tableName).where({[column]: value}).getExists();
-        return exists ? false : true;
-    }
+    const { tableName, column } = constraint;
 
-    defaultMessage(validationArguments?: ValidationArguments): string {
-        const column = validationArguments?.constraints[0]?.column;
-        return `The value for column '${column}' already exists. Please choose a different value.`;
-    }
+    const typedValue = value as string | number;
+
+    const exists: boolean = await this.entityManager
+      .getRepository(tableName)
+      .createQueryBuilder(tableName)
+      .where({ [column]: typedValue })
+      .getExists();
+
+    return !exists;
+  }
+
+  defaultMessage(validationArguments?: ValidationArguments): string {
+    const column = (
+      validationArguments?.constraints?.[0] as IsUniqueConstraintInput
+    )?.column;
+    return column
+      ? `The value for column '${column}' already exists. Please choose a different value.`
+      : 'This value already exists. Please choose a different value.';
+  }
 }
--- comics-platform-backend/src/validators/is-unique.ts
+++ comics-platform-backend/src/validators/is-unique.ts
@@ -1,20 +1,23 @@
-import { ValidationOptions, registerDecorator } from "class-validator";
-import { IsUniqueConstraint } from "./is-unique-constraint.validator";
+import { ValidationOptions, registerDecorator } from 'class-validator';
+import { IsUniqueConstraint } from './is-unique-constraint.validator';
 
 export type IsUniqueConstraintInput = {
-    tableName: string;
-    column: string;
-}
+  tableName: string;
+  column: string;
+};
 
-export function IsUnique(options: IsUniqueConstraintInput, validationOptions?: ValidationOptions){
-    return function (object: any, propertyName: string){
-        registerDecorator({
-            name: 'is-unique',
-            target: object.constructor,
-            propertyName: propertyName,
-            options: validationOptions,
-            constraints: [options],
-            validator: IsUniqueConstraint,
-        })
-    }
+export function IsUnique(
+  options: IsUniqueConstraintInput,
+  validationOptions?: ValidationOptions,
+) {
+  return function (object: Record<string, any>, propertyName: string) {
+    registerDecorator({
+      name: 'is-unique',
+      target: object.constructor,
+      propertyName: propertyName,
+      options: validationOptions,
+      constraints: [options],
+      validator: IsUniqueConstraint,
+    });
+  };
 }
