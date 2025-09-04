331a6fbd10377157294275892ee48edabae2002c
anisort <jaki.anisort@gmail.com>
2025-09-04 14:32:18 +0300

    erase unnecessary comments

--- comics-platform-backend/src/dto/create-comic.dto.ts
+++ comics-platform-backend/src/dto/create-comic.dto.ts
@@ -3,7 +3,6 @@ import { IsNotEmpty, IsString, IsEnum } from 'class-validator';
 export class CreateComicDto {
   @IsString()
   @IsNotEmpty()
-  //@IsUnique({tableName: 'comics', column: 'name'})
   name: string;
 
   @IsString()
