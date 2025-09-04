c9c20a8237eefed86a09d713faa44be097a5c896
anisort <jaki.anisort@gmail.com>
2025-09-04 16:46:41 +0300

    fix all ESLint issues in services/upload folder

--- comics-platform-backend/src/services/upload/upload.service.ts
+++ comics-platform-backend/src/services/upload/upload.service.ts
@@ -1,41 +1,43 @@
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
-import {v2} from 'cloudinary';
+import { v2 } from 'cloudinary';
 import { UploadApiResponse } from 'cloudinary';
 
 @Injectable()
 export class UploadService {
-
-  constructor(private configService: ConfigService){
+  constructor(private configService: ConfigService) {
     v2.config({
       cloud_name: this.configService.get('CLOUDINARY_CLOUD_NAME'),
       api_key: this.configService.get('CLOUDINARY_API_KEY'),
       api_secret: this.configService.get('CLOUDINARY_API_SECRET'),
     });
   }
-  
-  async upload(file: Express.Multer.File, folder: string): Promise<UploadApiResponse> { 
-    return await v2.uploader.upload(file.path, {folder});
+
+  async upload(
+    file: Express.Multer.File,
+    folder: string,
+  ): Promise<UploadApiResponse> {
+    return await v2.uploader.upload(file.path, { folder });
   }
 
   async delete(valueToDelete: string): Promise<void> {
     await v2.uploader.destroy(valueToDelete);
   }
 
-  async deletePageFromCloudService(imageUrl: string){
-    const publicId = (imageUrl).split('/').pop()?.split('.')[0];
+  async deletePageFromCloudService(imageUrl: string) {
+    const publicId = imageUrl.split('/').pop()?.split('.')[0];
     const valueToDelete = `comics-platform/comics/pages/${publicId}`;
     await this.delete(valueToDelete);
   }
 
   async deletePagesForEpisode(pageUrls: string[]): Promise<void> {
-    await Promise.all(pageUrls.map(p => this.deletePageFromCloudService(p)));
+    await Promise.all(pageUrls.map((p) => this.deletePageFromCloudService(p)));
   }
 
-  async deleteComicFromCloudService(coverUrl: string, pageUrls: string[]){
-    const publicId = (coverUrl).split('/').pop()?.split('.')[0];
+  async deleteComicFromCloudService(coverUrl: string, pageUrls: string[]) {
+    const publicId = coverUrl.split('/').pop()?.split('.')[0];
     const valueToDelete = `comics-platform/comics/covers/${publicId}`;
     await this.delete(valueToDelete);
-    await Promise.all(pageUrls.map(p => this.deletePageFromCloudService(p)));
+    await Promise.all(pageUrls.map((p) => this.deletePageFromCloudService(p)));
   }
 }
