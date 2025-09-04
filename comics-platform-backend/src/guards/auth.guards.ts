f11cebfb037b2346659a36d9696a80fbc677cb57
anisort <jaki.anisort@gmail.com>
2025-09-04 14:45:41 +0300

    fix all ESLint issues in guards folder

--- comics-platform-backend/src/guards/auth.guards.ts
+++ comics-platform-backend/src/guards/auth.guards.ts
@@ -1,17 +1,26 @@
-import { CanActivate, ExecutionContext, Injectable, UnauthorizedException} from '@nestjs/common';
+import {
+  CanActivate,
+  ExecutionContext,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
+import { RequestWithUser, UserPayload } from '../utils/user-payload';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
   constructor(private jwtService: JwtService) {}
 
   async canActivate(ctx: ExecutionContext): Promise<boolean> {
-    const req = ctx.switchToHttp().getRequest();
-    const token = req.headers.authorization?.split(' ')[1];
+    const req: RequestWithUser = ctx
+      .switchToHttp()
+      .getRequest<RequestWithUser>();
+    const authHeader: string | undefined = req.headers.authorization;
+    const token: string | undefined = authHeader?.split(' ')[1];
     if (!token) throw new UnauthorizedException();
 
     try {
-      const payload = await this.jwtService.verifyAsync(token);
+      const payload = await this.jwtService.verifyAsync<UserPayload>(token);
       req.user = { userId: payload.userId, username: payload.username };
       return true;
     } catch {
