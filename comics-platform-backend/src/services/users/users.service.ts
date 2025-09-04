effd39188b11841094950a6c0f718db1dcfb5d4f
anisort <jaki.anisort@gmail.com>
2025-09-04 16:47:05 +0300

    fix all ESLint issues in services/users folder

--- comics-platform-backend/src/services/users/users.service.ts
+++ comics-platform-backend/src/services/users/users.service.ts
@@ -5,20 +5,16 @@ import { Repository } from 'typeorm';
 
 @Injectable()
 export class UsersService {
-
   constructor(
-      @InjectRepository(User)
-      private usersRepository: Repository<User>
+    @InjectRepository(User)
+    private usersRepository: Repository<User>,
   ) {}
 
   async checkUsernameOrEmail(value: string): Promise<{ exists: boolean }> {
-      const user = await this.usersRepository.findOne({
-        where: [
-          { username: value },
-          { email: value },
-        ],
-      });
-      return { exists: user ? true : false };
+    const user = await this.usersRepository.findOne({
+      where: [{ username: value }, { email: value }],
+    });
+    return { exists: !!user };
   }
 
   async findUserByName(username: string): Promise<User | null> {
@@ -26,20 +22,20 @@ export class UsersService {
       where: { username },
     });
   }
-  
+
   async findUserByIdWithSubscriptions(userId: number): Promise<User> {
     const user = await this.usersRepository.findOne({
       where: { id: userId },
       relations: ['subscribedComics'],
     });
-  
+
     if (!user) {
       throw new NotFoundException('User not found');
     }
-  
+
     return user;
   }
-  
+
   async saveUser(user: User): Promise<User> {
     return await this.usersRepository.save(user);
   }
@@ -47,6 +43,4 @@ export class UsersService {
   async findUserById(id: number): Promise<User | null> {
     return this.usersRepository.findOne({ where: { id } });
   }
-  
-  
 }
