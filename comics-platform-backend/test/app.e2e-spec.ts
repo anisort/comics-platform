32a22f5705a572cbdda40032fe826015bad06de4
anisort <jaki.anisort@gmail.com>
2025-09-05 09:58:11 +0300

    fix all ESLint issues in app.e2e-spec.ts

--- comics-platform-backend/test/app.e2e-spec.ts
+++ comics-platform-backend/test/app.e2e-spec.ts
@@ -1,39 +1,11 @@
-// import { Test, TestingModule } from '@nestjs/testing';
-// import { INestApplication } from '@nestjs/common';
-// import * as request from 'supertest';
-// import { App } from 'supertest/types';
-// import { AppModule } from './../src/app.module';
-
-// describe('AppController (e2e)', () => {
-//   let app: INestApplication<App>;
-
-//   beforeEach(async () => {
-//     const moduleFixture: TestingModule = await Test.createTestingModule({
-//       imports: [AppModule],
-//     }).compile();
-
-//     app = moduleFixture.createNestApplication();
-//     await app.init();
-//   });
-
-//   it('/ (GET)', () => {
-//     return request(app.getHttpServer())
-//       .get('/')
-//       .expect(200)
-//       .expect('Hello World!');
-//   });
-// });
-
-
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication, ValidationPipe } from '@nestjs/common';
 import * as request from 'supertest';
+import type { Server } from 'http';
 import { AppModule } from '../src/app.module';
-import { DataSource } from 'typeorm';
 
 describe('Comics (e2e)', () => {
   let app: INestApplication;
-  let dataSource: DataSource;
   let jwtToken: string;
 
   beforeAll(async () => {
@@ -45,28 +17,26 @@ describe('Comics (e2e)', () => {
     app.useGlobalPipes(new ValidationPipe());
     await app.init();
 
-    dataSource = moduleFixture.get(DataSource);
-
-    const loginRes = await request(app.getHttpServer())
+    const loginRes: { body: { accessToken: string } } = await request(
+      app.getHttpServer() as unknown as Server,
+    )
       .post('/auth/login')
       .send({
-        username: 'anisort',
+        username: 'test',
         password: 'Test100#',
       });
 
     jwtToken = loginRes.body.accessToken;
-
   });
 
   afterAll(async () => {
     await app.close();
   });
 
-  it('should show concrete user\'s comics', () => {
-    return request(app.getHttpServer())
-      .get('/comics/my-library')
+  it("should show concrete user's comics", async () => {
+    return request(app.getHttpServer() as unknown as Server)
+      .get('/comics/own-comics')
       .set('Authorization', `Bearer ${jwtToken}`)
       .expect(200);
-  })
-    
+  });
 });
